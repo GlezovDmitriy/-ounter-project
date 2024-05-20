4a08f56b18f60c26a991668c9542deaadcc17ad1
GlezovDmitriy <dmitriy_glezov@mail.ru>
2024-05-20 23:44:42 +0300

    added local storage in Redux

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -11,6 +11,11 @@ const rootReducer = combineReducers({
 export type AppStateType = ReturnType<typeof rootReducer>
 // непосредственно создаём store
 export const store = createStore(rootReducer)
+
+store.subscribe(()=>{
+    localStorage.setItem('app-state', JSON.stringify(store.getState()))
+    localStorage.setItem('count', JSON.stringify(store.getState().counter?.count))
+})
 // определить автоматически тип всего объекта состояния
 export type AppStoreType = typeof store
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
